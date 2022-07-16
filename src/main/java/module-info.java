16b51c03b3d646fd265040265ba340d0688a6aa5
Luiza Vieira <luuhbam@gmail.com>
2022-07-15 22:15:11 -0300

    Second commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javapoodio {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javapoodio to javafx.fxml;
-    exports com.example.javapoodio;
-}
